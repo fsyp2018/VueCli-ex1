cdefab6145bd3e15cd08d8ad836bd198fe9b0eef
fsyp2018 <45746305+fsyp2018@users.noreply.github.com>
2019-06-03 19:27:57 +0800

    Add files via upload

---
+++ src/components/deproducts.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+<div
+      class="modal fade"
+      id="delProductModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content border-0">
+          <div class="modal-header bg-danger text-white">
+            <h5 class="modal-title" id="exampleModalLabel">
+              <span>刪除產品</span>
+            </h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            是否刪除
+            <strong class="text-danger">{{ tempProduct.title }}</strong> 商品(刪除後將無法恢復)。
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">取消</button>
+            <button type="button" class="btn btn-danger" @click="Confirm">確認刪除</button>
+          </div>
+        </div>
+      </div>
+    </div> 
+  </div>
+</template> 
+
+<script>
+
+export default {
+  props:{
+    tempProduct:{
+        type:Object,
+        default: {}        
+    }
+  },
+  methods:{
+    Confirm(){
+      this.$emit('Confirm')
+    },
+    
+  }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,55 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+// import HelloWorld from '@/components/HelloWorld';
+import Dashboard from '@/components/Dashboard';
+import Login from '@/components/pages/Login';
+import Products from '@/components/pages/Products';
+import CustomerOrder from '@/components/pages/CustomerOrder';
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path:'*',
+      redirect:'/login',
+    },
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld,
+    //   meta: { requiresAuth: true },
+    // },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+    },
+    {
+      path: '/admin',
+      name: 'Dashboard',
+      component: Dashboard,
+      children :[
+        {
+          path: 'products',
+          name: 'Products',
+          component: Products,
+          meta: { requiresAuth: true },
+        }
+      ]
+    },
+    {
+      path: '/',
+      name: 'Dashboard',
+      component: Dashboard,
+      children :[
+        {
+          path: 'customer_order',
+          name: 'CustomerOrder',
+          component: CustomerOrder,          
+        }
+      ]
+    },
+
+  ]
+})
